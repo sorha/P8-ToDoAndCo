fdbf9a0240ecf276aeee7e2b58eaa990889b63af
Sorha <sorha.entropy@gmail.com>
2019-03-18 11:21:12 +0100

    4.2 Deprecations fixes

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,9 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class DefaultController extends Controller
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
