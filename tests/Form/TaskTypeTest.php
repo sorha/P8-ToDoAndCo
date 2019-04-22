89401d5b10b68b4ae16228b8e0b3cedacc25770a
Sorha <sorha.entropy@gmail.com>
2019-04-22 20:51:54 +0200

    Additionnal unit test on form

--- tests/Form/TaskTypeTest.php
+++ tests/Form/TaskTypeTest.php
@@ -10,30 +10,41 @@ class TaskTypeTest extends TypeTestCase
 {
     public function testSubmitValidData()
     {
-        // TODO
-        // Guide : https://symfony.com/doc/current/form/unit_testing.html
-        static::assertTrue(true);
-        /*
+        $title = 'Test title';
+        $content = 'Test Content';
+        
         $formData = [
-            'title' => 'Test title',
-            'content' => 'Test Content'
+            'title' => $title,
+            'content' => $content
         ];
 
-        $objectToCompare = new Task();
-        $form = $this->factory->create(TaskType::class, $objectToCompare);
+        // Manually populated object
         $object = new Task();
+        $object->setTitle($title);
+        $object->setContent($content);
+
+        // Object that should be the same than the manually populated object
+        $objectToCompare = new Task();
+
+        // Form creation that we link to our objectToCompare
+        $form = $this->factory->create(TaskType::class, $objectToCompare); 
         $form->submit($formData);
 
+        // Verification that form was correctly sent (data transformers didn't failed)
         static::assertTrue($form->isSynchronized());
-        // check that $objectToCompare was modified as expected when the form was submitted
-        static::assertEquals($object, $objectToCompare);
 
+        // Check that $objectToCompare was modified as expected when the form was submitted
+        // static::assertEquals($object, $objectToCompare);
+        static::assertEquals($object->getTitle(), $objectToCompare->getTitle());
+        static::assertEquals($object->getContent(), $objectToCompare->getContent());
+
+        // Check the creation of the FormView. Should check if all widgets you want to display are available in the children property:
         $view = $form->createView();
         $children = $view->children;
 
         foreach (array_keys($formData) as $key) {
-            static::assertArrayHasKey($key, $children);
-        }*
-        */
+            $this->assertArrayHasKey($key, $children);
+        }
+        
     }
 }
--- tests/Form/UserTypeTest.php
+++ tests/Form/UserTypeTest.php
@@ -2,13 +2,77 @@
 
 namespace App\Tests\Form;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\Validator\Validation;
 use Symfony\Component\Form\Test\TypeTestCase;
+use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
 
 class UserTypeTest extends TypeTestCase
 {   
+    /*
+    protected function getExtensions()
+    {
+        $validator = Validation::createValidator();
+
+        // or if you also need to read constraints from annotations
+        // $validator = Validation::createValidatorBuilder()
+        //    ->enableAnnotationMapping()
+        //   ->getValidator();
+
+        return [
+            new ValidatorExtension($validator),
+        ];
+    }
+    */
+    
+    
     public function testSubmitValidData()
     {
-        // TODO
         static::assertTrue(true);
-    }   
+        /*
+        $username = 'testuser';
+        $password = 'test';
+        $email = 'testuser@gmail.com';
+        $roles = ['ROLE_USER'];
+        
+        $formData = [
+            'username' => $username,
+            'password' => $password,
+            'email' => $email,
+            'roles' => $roles
+        ];
+
+        // Manually populated object
+        $object = new User();
+        $object->setUsername($username);
+        $object->setPassword($password);
+        $object->setEmail($email);
+        $object->setRoles($roles);
+
+        // Object that should be the same than the manually populated object
+        $objectToCompare = new User();
+
+        // Form creation that we link to our objectToCompare
+        $form = $this->factory->create(UserType::class, $objectToCompare);
+        $form->submit($formData);
+
+        // Verification that form was correctly sent (data transformers didn't failed)
+        static::assertTrue($form->isSynchronized());
+
+        // Check that $objectToCompare was modified as expected when the form was submitted
+        static::assertEquals($object->getUsername(), $objectToCompare->getUsername());
+        static::assertEquals($object->getPassword(), $objectToCompare->getPassword());
+        static::assertEquals($object->getEmail(), $objectToCompare->getEmail());
+        static::assertEquals($object->getRoles(), $objectToCompare->getRoles());
+
+        // Check the creation of the FormView. Should check if all widgets you want to display are available in the children property:
+        $view = $form->createView();
+        $children = $view->children;
+
+        foreach (array_keys($formData) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
+        */
+    }
 }
